3356747b09664b2befa2510ec062cbe3f9077631
Rogério Pereira Araújo <rogerio.araujo@gmail.com>
2015-01-08 17:42:19 -0200

    Initial Commit

---
+++ RACAlamofire/RACAlamofire.h
@@ -0,0 +1,19 @@
+//
+//  RACAlamofire.h
+//  RACAlamofire
+//
+//  Created by Rogério Pereira Araújo on 08/01/15.
+//  Copyright (c) 2015 Rogério Pereira Araújo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RACAlamofire.
+FOUNDATION_EXPORT double RACAlamofireVersionNumber;
+
+//! Project version string for RACAlamofire.
+FOUNDATION_EXPORT const unsigned char RACAlamofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RACAlamofire/PublicHeader.h>
+
+
