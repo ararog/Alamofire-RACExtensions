23e35bd10a18577fc0adb634f236cbfdf26ef2c4
Rogério Pereira Araújo <rogerio.araujo@gmail.com>
2015-01-08 17:46:49 -0200

    Initial commit of code

---
+++ RACAlamofire/RACAlamofire.swift
@@ -0,0 +1,9 @@
+//
+//  RACAlamofire.swift
+//  RACAlamofire
+//
+//  Created by Rogério Pereira Araújo on 08/01/15.
+//  Copyright (c) 2015 Rogério Pereira Araújo. All rights reserved.
+//
+
+import Foundation
